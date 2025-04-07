42929bbcbca8ba0049ce40c690458dbea6c9b278
Eric Licea Tapia <ericliceatapia@outlook.com>
2025-04-07 10:40:17 -0500

    Fixed minor issues when running app.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
 
       <HelloWorld msg="Hello, World!" />
