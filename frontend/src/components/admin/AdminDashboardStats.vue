0772eca4fbcbaec98c04e9636f23befdb46490ab
Eric Licea Tapia <ericliceatapia@outlook.com>
2025-04-29 04:05:47 -0500

    Admin dashboard complete

---
+++ frontend/src/components/admin/AdminDashboardStats.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex gap-8 justify-center">
+    <div class="bg-white p-6 rounded shadow w-48 text-center">
+      <h3 class="text-lg font-bold">Crew Members</h3>
+      <p class="text-3xl">{{ crewCount }}</p>
+    </div>
+    <div class="bg-white p-6 rounded shadow w-48 text-center">
+      <h3 class="text-lg font-bold">Games</h3>
+      <p class="text-3xl">{{ gameCount }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  crewCount: {
+    type: Number,
+    required: true,
+  },
+  gameCount: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
--- frontend/src/views/admin/AdminHomeView.vue
+++ frontend/src/views/admin/AdminHomeView.vue
@@ -1,10 +1,44 @@
-<script setup></script>
-
 <!-- Route: /admin -->
 
 <template>
-  <div class="p-4">
-    <h1 class="text-3xl font-bold mb-4">Admin Dashboard</h1>
-    <p>Welcome, Admin!</p>
+  <div class="max-w-4xl mx-auto py-8 space-y-8">
+    <LoadingSpinner v-if="loading" message="Loading your dashboard..." />
+
+    <div v-else class="space-y-8">
+      <h1 class="text-3xl font-bold mb-4">My Dashboard</h1>
+
+      <AdminDashboardStats
+        :crewCount="crewMembers.length"
+        :gameCount="games.length"
+      />
+      <AdminUpcomingGames :games="games" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getUsersByRole } from '@/apis/users'
+import { getAllGames } from '@/apis/games'
+
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue'
+import AdminDashboardStats from '@/components/admin/AdminDashboardStats.vue'
+import AdminUpcomingGames from '@/components/admin/AdminUpcomingGames.vue'
+
+const crewMembers = ref([])
+const games = ref([])
+const loading = ref(true)
+
+onMounted(async () => {
+  try {
+    crewMembers.value = await getUsersByRole('CREW_MEMBER')
+    games.value = await getAllGames()
+  } catch (err) {
+    console.error('Error loading dashboard:', err)
+  } finally {
+    window.setInterval(() => {
+      loading.value = false
+    }, 1000)
+  }
+})
+</script>
